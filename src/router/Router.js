076684c62498791fb4c8ed9e23373305716292d6
Moore-ConnorS <littlemoorepower@Connors-MacBook-Pro.local>
2019-01-17 17:40:55 -0700

    delete old code

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Dashboard from './../components/dashboard/Dashboard';
+
 import Marketplace from './../components/marketplace/Marketplace';
 import SinglePhoto from './../components/singlePhoto/SinglePhoto';
 import Cart from './../components/cart/Cart';
